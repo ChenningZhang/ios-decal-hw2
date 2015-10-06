fcab8a4350ae7ccc7bb00d62acd79d70fdfe64fb
Chenning Zhang <czhang1306@berkeley.edu>
2015-10-06 10:15:24 -0700

    Fixed buttons issues. Clicking doesn't work still.

--- CustomKeyboard/CalKeyboard/KeyboardViewController.swift
+++ CustomKeyboard/CalKeyboard/KeyboardViewController.swift
@@ -13,18 +13,14 @@ class KeyboardViewController: UIInputViewController {
     @IBOutlet var nextKeyboardButton: UIButton!
     @IBOutlet var deleteKeyboardButton: UIButton!
     @IBOutlet var returnKeyboardButton: UIButton!
-    @IBOutlet var firstKeyboardButton: UIButton!
-    @IBOutlet var secondKeyboardButton: UIButton!
-    @IBOutlet var thirdKeyboardButton: UIButton!
-    @IBOutlet var fourthKeyboardButton: UIButton!
-    @IBOutlet var fifthKeyboardButton: UIButton!
-    @IBOutlet var sixthKeyboardButton: UIButton!
-    @IBOutlet var seventhKeyboardButton: UIButton!
-    @IBOutlet var eighthKeyboardButton: UIButton!
-    @IBOutlet var ninthKeyboardButton: UIButton!
-    @IBOutlet var tenKeyboardButton: UIButton!
-    @IBOutlet var elevenKeyboardButton: UIButton!
-    @IBOutlet var tweleveKeyboardButton: UIButton!
+    @IBOutlet var oneButton: UIButton!
+    @IBOutlet var twoButton: UIButton!
+    @IBOutlet var threeButton: UIButton!
+    @IBOutlet var fourButton: UIButton!
+    @IBOutlet var fiveButton: UIButton!
+    @IBOutlet var sixButton: UIButton!
+    @IBOutlet var sevenButton: UIButton!
+    @IBOutlet var eightButton: UIButton!
     
     
     var keyboardView: UIView!
@@ -62,64 +58,64 @@ class KeyboardViewController: UIInputViewController {
         nextKeyboardButton.addTarget(self, action: "advanceToNextInputMode", forControlEvents: .TouchUpInside) // advanceToNextInputMode is already defined in template
         deleteKeyboardButton.addTarget(self, action: "deleteAction", forControlEvents: .TouchUpInside)
         returnKeyboardButton.addTarget(self, action: "returnAction", forControlEvents: .TouchUpInside)
-        firstKeyboardButton.addTarget(self, action: "inputAction", forControlEvents: .TouchUpInside)
-        secondKeyboardButton.addTarget(self, action: "inputAction", forControlEvents: .TouchUpInside)
-        thirdKeyboardButton.addTarget(self, action: "inputAction", forControlEvents: .TouchUpInside)
-        fourthKeyboardButton.addTarget(self, action: "inputAction", forControlEvents: .TouchUpInside)
-        fifthKeyboardButton.addTarget(self, action: "inputAction", forControlEvents: .TouchUpInside)
-        sixthKeyboardButton.addTarget(self, action: "inputAction", forControlEvents: .TouchUpInside)
-        seventhKeyboardButton.addTarget(self, action: "inputAction", forControlEvents: .TouchUpInside)
-        eighthKeyboardButton.addTarget(self, action: "inputAction", forControlEvents: .TouchUpInside)
-        ninthKeyboardButton.addTarget(self, action: "inputAction", forControlEvents: .TouchUpInside)
-        tenKeyboardButton.addTarget(self, action: "inputAction", forControlEvents: .TouchUpInside)
-        elevenKeyboardButton.addTarget(self, action: "inputAction", forControlEvents: .TouchUpInside)
-        tweleveKeyboardButton.addTarget(self, action: "inputAction", forControlEvents: .TouchUpInside)
+        oneButton.addTarget(self, action: "inputOne", forControlEvents: .TouchUpInside)
+        twoButton.addTarget(self, action: "inputTwo", forControlEvents: .TouchUpInside)
+        threeButton.addTarget(self, action: "inputThree", forControlEvents: .TouchUpInside)
+        fourButton.addTarget(self, action: "inputFour", forControlEvents: .TouchUpInside)
+        fiveButton.addTarget(self, action: "inputFive", forControlEvents: .TouchUpInside)
+        sixButton.addTarget(self, action: "inputSix", forControlEvents: .TouchUpInside)
+        sevenButton.addTarget(self, action: "inputSeven", forControlEvents: .TouchUpInside)
+        eightButton.addTarget(self, action: "inputEight", forControlEvents: .TouchUpInside)
         
     }
     
-    func inputAction(sender: AnyObject) {
-        let button = sender as! UIButton
-        var input = ""
-        let proxy = self.textDocumentProxy as UIKeyInput
-        switch(button.tag) {
-        case 1:
-            input = "♔"
-        case 2:
-            input = "☾"
-        case 3:
-            input = "☎"
-        case 4:
-            input = "❀"
-        case 5:
-            input = "☯"
-        case 6:
-            input = "ღ"
-        case 7:
-            input = "★"
-        case 8:
-            input = "♖"
-        case 9:
-            input = "☃"
-        case 10:
-            input = "☁"
-        case 11:
-            input = "⌘"
-        case 12:
-            input = "❦"
-        default:
-            input = ""
-            
-        }
-        proxy.insertText(input)
+    func inputOne(sender: AnyObject) {
+        let proxy = self.textDocumentProxy as UITextDocumentProxy
+        proxy.insertText("1")
+    }
+    
+    func inputTwo(sender: AnyObject) {
+        let proxy = self.textDocumentProxy as UITextDocumentProxy
+        proxy.insertText("2")
+    }
+    
+    func inputThree(sender: AnyObject) {
+        let proxy = self.textDocumentProxy as UITextDocumentProxy
+        proxy.insertText("3")
+    }
+    
+    func inputFour(sender: AnyObject) {
+        let proxy = self.textDocumentProxy as UITextDocumentProxy
+        proxy.insertText("4")
+    }
+    
+    func inputFive(sender: AnyObject) {
+        let proxy = self.textDocumentProxy as UITextDocumentProxy
+        proxy.insertText("5")
+    }
+
+    func inputSix(sender: AnyObject) {
+        let proxy = self.textDocumentProxy as UITextDocumentProxy
+        proxy.insertText("6")
+    }
+
+    func inputSeven(sender: AnyObject) {
+        let proxy = self.textDocumentProxy as UITextDocumentProxy
+        proxy.insertText("7")
+    }
+
+    func inputEight(sender: AnyObject) {
+        let proxy = self.textDocumentProxy as UITextDocumentProxy
+        proxy.insertText("8")
     }
     
     func deleteAction(sender: AnyObject) {
-        let proxy = self.textDocumentProxy as UIKeyInput
+        let proxy = self.textDocumentProxy as UITextDocumentProxy
         proxy.deleteBackward()
     }
     
     func returnAction(sender: AnyObject) {
-        let proxy = self.textDocumentProxy as UIKeyInput
+        let proxy = self.textDocumentProxy as UITextDocumentProxy
         proxy.insertText("\n")
     }
 
